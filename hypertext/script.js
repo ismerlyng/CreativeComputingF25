dd6e43d87b26c8c3596ea6cf4257c5c702f0db34
ismerlyng <145296948+ismerlyng@users.noreply.github.com>
2025-04-09 16:15:21 -0400

    Delete hypertext/script.js

--- hypertext/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("scroll", () => {
-    let scrollPosition = window.scrollY;
-    
-    document.getElementById("layer1").style.transform = `translateY(${scrollPosition * 0.3}px)`;
-    document.getElementById("layer2").style.transform = `translateY(${scrollPosition * 0.5}px)`;
-    document.getElementById("layer3").style.transform = `translateY(${scrollPosition * 0.7}px)`;
-});
